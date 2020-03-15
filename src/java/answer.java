32c1c4a533a8fb95c16d573f89c7d1eb3c4a0329
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-03-15 18:27:49 +0700

    'commit'

--- src/java/answer.java
+++ src/java/answer.java
@@ -7,13 +7,24 @@ import org.json.simple.*;
 public class answer extends HttpServlet {
 
     private String answer(String question) {
-       String[] quest = 
-       { "Tôi muốn biết phương thức tuyển sinh của đại học fpt ?",
-           "Bạn có thể giải thích rõ hơn về điều kiện miễn thi sơ tuyển của Trường đại học FPT không ?",
-           "Bạn có thể giải thích rõ hơn về kỳ thi sơ tuyển của Trường đại học FPT không ?",
-           "tôi hiểu rồi cám ơn bạn .",
-       }; 
+        String[] quest
+                = {"FPT tuyển sinh như thế nào ?",
+                    "Điều kiện miễn thi sơ tuyển là như thế nào ?",
+                    "Thi sơ tuyển ở FPT như thế nào ?",
+                    "OK cảm ơn nhé .",};
         String answer = null;
+
+        if (question.equalsIgnoreCase(quest[0])) {
+            answer = "Các thí sinh thuộc đối tượng tuyển sinh có nguyện vọng theo học tại Trường đại học FPT cần đáp ứng đủ 2 tiêu chí :  tham dự và trúng tuyển kỳ thi sơ tuyển của Trường đại học FPT hoặc đủ điều kiện miễn thi sơ tuyển của Trường đại học FPT.";
+        } else if (question.equalsIgnoreCase(quest[1])) {
+            answer = "Để được miễn sơ tuyển thì tổng điểm 3 môn  (mỗi môn tính trung bình hai học kỳ cuối truung học phổ thông) của bạn cần  đạt 18 điểm trở lên xét theo tổ hợp môn tương ứng với ngành đăng ký học tại Trường đại học FPT.";
+        } else if (question.equalsIgnoreCase(quest[2])) {
+            answer = "Để trúng tuyển vào đại học fpt thì tổng điểm 3 môn trong kỳ thi trung học phổ thông đạt 15 điểm trở lên (đã bao gồm điểm ưu tiên theo quy định của Bộ Giáo dục và Đào tạo) xét theo tổ hợp môn tương ứng với ngành đăng ký học tại Trường đại học  FPT.";
+        } else if (question.equalsIgnoreCase(quest[3])) {
+            answer = "Vâng mọi thắc mắc bạn có thể liên hệ cho tôi hoặc theo đường dây nóng  0 2 4 7 3 0 0 5 5 8 8";
+        } else {
+            answer = "Câu hỏi của bạn nằm ngoài kiến thức của tôi mất rồi.";
+        }
         return answer;
     }
 
@@ -31,6 +42,7 @@ public class answer extends HttpServlet {
         PrintWriter out = res.getWriter();
         StringBuffer sb = new StringBuffer();
         String line = null;
+        String answer = null; 
         //get data from client side using buffer 
         try {
             BufferedReader reader = req.getReader();
@@ -40,18 +52,19 @@ public class answer extends HttpServlet {
         } catch (Exception e) {
             System.out.println("BufferedReader Error: " + e);
         }
-//        String s = "{\"name\":\"sonoo\",\"salary\":600000.0,\"age\":27}";
+        //convert json object to string
         Object obj = JSONValue.parse(sb.toString());
         JSONObject jsonObject = (JSONObject) obj;
+        //debug line 1 
         System.out.println(jsonObject + " ----- line 1 ");
+        //get property question from client-side
+        //the data from client is {"question": "user's question"}
         String quest = (String) jsonObject.get("question");
+        //debug line 2
         System.out.println(quest + " ----- line 2 ");
-        if (quest.equalsIgnoreCase("Thế nào ?")) {
-
-            System.out.println("Success" + " ----- line 3 ");
-        }
-        out.print(quest + " ----- line 4 ");
-        answer(quest);
+        answer = answer(quest);
+        //response to client an answer 
+        out.print(answer);
     }
 
     @Override
